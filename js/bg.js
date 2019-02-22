2d247fd1834d4ff29d1a59ad8af8e6abdfcbf353
Noah <savedream75@gmail.com>
2019-02-22 14:36:22 +0900

    Add Background

---
+++ js/bg.js
@@ -0,0 +1,18 @@
+const body = document.querySelector("body");
+
+const MAX_IMG_NUMBER = 5;
+
+function setImage() {
+  const randomNumber = Math.floor(Math.random() * MAX_IMG_NUMBER) + 1;
+  const image = new Image();
+  image.src = `images/${randomNumber}.jpg`;
+  image.classList.add('bgImage');
+  body.prepend(image);
+  console.log(image.clientWidth, image.clientHeight);
+}
+
+function init() {
+  setImage();
+}
+
+init();
