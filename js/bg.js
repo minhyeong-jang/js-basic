0e2490797bd65cff155fa7706683efa7fb02907d
Noah <savedream75@gmail.com>
2019-03-24 17:21:36 +0900

    Update Background Image Parser

--- js/bg.js
+++ js/bg.js
@@ -1,17 +1,36 @@
-const body = document.querySelector("body");
+const clientId = "8bca8a1caaa60bf9756e8bfa221a3964bf23712b7e577b122d02e1519d21c56b";
+const clientSecret = "e90293306216a1b47f4a41b0f70422e8eaa7fbdb5d3f2045ef4b5df5b1a9d02d";
+const date = new Date();
+const BACKGROUND_LS = `${date.getFullYear()}${date.getMonth()}${date.getDay()}-background`;
+const background = document.querySelector(".js-background"),
+  image = background.querySelector(".js-image");
 
 const MAX_IMG_NUMBER = 5;
 
-function setImage() {
-  const randomNumber = Math.floor(Math.random() * MAX_IMG_NUMBER) + 1;
-  const image = new Image();
-  image.src = `images/${randomNumber}.jpg`;
-  image.classList.add('bgImage');
-  body.prepend(image);
+function setImage(url) {
+  image.style.backgroundImage = `url(${url})`;
+}
+
+function getImage() {
+  fetch(`https://api.unsplash.com/photos/random?client_id=${clientId}&client_secret=${clientSecret}&count=1&query=landscape`)
+    .then(res => res.json())
+    .then(json => {
+      setImage(json[0].urls.regular);
+      localStorage.setItem(BACKGROUND_LS, JSON.stringify(json[0]));
+    }); 
+}
+
+function loadedImage() {
+  const bgData = localStorage.getItem(BACKGROUND_LS);
+  if(!bgData) {
+    getImage();
+  } else {
+    setImage(JSON.parse(bgData).urls.regular);
+  }
 }
 
 function init() {
-  setImage();
+  loadedImage();
 }
 
 init();
